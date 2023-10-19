d435d9b15998c699baa054b9d453148b428fa7a9
Oormi <ochakravarty@gmail.com>
2023-10-19 16:10:25 +1100

    Condensed some code. Still some errors to fix

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "1178834df81505d3a6328764b26b4da3"
+
+# Geoapify API Key
+geoapify_key = "bc436cbe9c274d6f92db1acacb620a9c"
+
+# Oormi's API Key
 apikey = "a6c6fe77e8154fa7a2f0daaf2492f581"
